6264305c27beff64c4890b272bd101a3b58b1ddf
SongSol <thdthf159@naver.com>
2018-03-28 20:47:22 +0900

    get gps

---
+++ app/src/main/java/kr/ac/yjc/wdj/myapplication/GetGPS.java
@@ -0,0 +1,99 @@
+package kr.ac.yjc.wdj.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ToggleButton;
+
+public class GetGPS extends AppCompatActivity {
+    TextView tv;
+    ToggleButton tb;
+    double longitude;   //경도
+    double latitude;    //위도
+    double altitude;    //고도
+    float accuracy;     //정확도
+    String provider;    //위치제공자
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_gpsinfo);
+
+        // Location 제공자에서 정보를 얻어오기(GPS)
+        // 1. Location을 사용하기 위한 권한을 얻어와야한다 AndroidManifest.xml
+        //     ACCESS_FINE_LOCATION : NETWORK_PROVIDER, GPS_PROVIDER
+        //     ACCESS_COARSE_LOCATION : NETWORK_PROVIDER
+        // 2. LocationManager 를 통해서 원하는 제공자의 리스너 등록
+        // 3. GPS 는 에뮬레이터에서는 기본적으로 동작하지 않는다
+        // 4. 실내에서는 GPS_PROVIDER 를 요청해도 응답이 없다.  특별한 처리를 안하면 아무리 시간이 지나도
+        //    응답이 없다.
+        //    해결방법은
+        //     ① 타이머를 설정하여 GPS_PROVIDER 에서 일정시간 응답이 없는 경우 NETWORK_PROVIDER로 전환
+        //     ② 혹은, 둘다 한꺼번헤 호출하여 들어오는 값을 사용하는 방식.
+
+        tv = (TextView)findViewById(R.id.textView2);
+        tv.setText("미수신중");
+
+        tb = (ToggleButton)findViewById(R.id.toggle1);
+
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    if(tb.isChecked()){
+                        tv.setText("수신중..");
+                        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                                100, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                                100, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                    }else{
+                        tv.setText("미수신중");
+                        lm.removeUpdates(mLocationListener);
+                    }
+                }catch(SecurityException ex){
+                }
+            }
+        });
+    } // end of onCreate
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            longitude = location.getLongitude(); //경도
+            latitude = location.getLatitude();   //위도
+            altitude = location.getAltitude();   //고도
+            accuracy = location.getAccuracy();    //정확도
+            provider = location.getProvider();   //위치제공자
+
+            tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
+                    + "\n고도 : " + altitude + "\n정확도 : "  + accuracy);
+        }
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
+}
