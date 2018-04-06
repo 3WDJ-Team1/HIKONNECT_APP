20f1a14e2d833c42fe4d93f45eaf709b7672e2e5
SongSol <thdthf159@naver.com>
2018-04-06 11:48:38 +0900

    Login

--- app/src/main/java/kr/ac/yjc/wdj/myapplication/LoginActivity.java
+++ app/src/main/java/kr/ac/yjc/wdj/myapplication/LoginActivity.java
@@ -1,25 +1,34 @@
 package kr.ac.yjc.wdj.myapplication;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.dd.processbutton.ProcessButton;
 import com.gc.materialdesign.views.ButtonRectangle;
 import com.google.android.gms.tasks.OnCompleteListener;
+
+import kr.ac.yjc.wdj.myapplication.APIs.HttpRequest.HttpRequestConnection;
 
 /**
  * Created by jungyu on 2018-04-05.
  */
 
 public class LoginActivity extends Activity  {
-    EditText id;
-    EditText pw;
+    EditText id,pw;
+    TextView tv;
     ButtonRectangle login;
+    ContentValues contentValues = new ContentValues();
+    HttpRequestConnection hrc = new HttpRequestConnection();
+    String result;
+    Handler handler;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +36,37 @@ public class LoginActivity extends Activity  {
 
         id = findViewById(R.id.idtext);
         pw = findViewById(R.id.pwtext);
+        tv = findViewById(R.id.tv);
         login = findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,MapsActivity.class);
-                startActivity(intent);
+                try {
+                    contentValues.put("id",id.getText().toString());
+                    contentValues.put("pw",pw.getText().toString());
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            result = hrc.request("http://172.26.2.249:8000/api/login_app", contentValues);
+                            Message msg = handler.obtainMessage();
+                            handler.sendMessage(msg);
+                        }
+                    }).start();
+                    handler = new Handler() {
+                        public void handleMessage(Message msg) {
+                        switch (result) {
+                            case "true":
+                                Intent intent = new Intent(LoginActivity.this,MapsActivity.class);
+                                startActivity(intent);
+                                break;
+                            case "false":
+                                tv.setText("존재하지 않는 ID이거나 비밀번호가 다릅니다.");
+                        }
+                        }
+                    };
+                }catch (SecurityException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
     }
