f5b4a894e64ae0bb87d8d7971d4c31ed16fbba95
Choijungyu <wnsrb0147@naver.com>
2018-07-16 19:25:41 +0900

    레이아웃 수정

--- app/src/main/java/kr/ac/yjc/wdj/hikonnect/AfterHikingActivity.java
+++ app/src/main/java/kr/ac/yjc/wdj/hikonnect/AfterHikingActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -69,6 +71,12 @@ public class AfterHikingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_after_hiking);
 
         pref = getSharedPreferences("loginData", MODE_PRIVATE);
+
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.mainColor));
+
 
         // UI 초기화
         tvRemainMembers     = findViewById(R.id.howManyNowHiking);
